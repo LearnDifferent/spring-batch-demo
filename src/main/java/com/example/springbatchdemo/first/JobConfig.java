f6fe20548c6250039af05939454d69529706235f
Zhou <zhou.h.p@outlook.com>
2023-03-28 12:51:02 +0800

    learn: Hello World

---
+++ src/main/java/com/example/springbatchdemo/first/JobConfig.java
@@ -0,0 +1,57 @@
+package com.example.springbatchdemo.first;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Hello World 配置
+ *
+ * @author zhou
+ * @date 2023/3/28
+ */
+@Configuration
+@EnableBatchProcessing
+public class JobConfig {
+
+    private final JobBuilderFactory jobBuilderFactory;
+
+    private final StepBuilderFactory stepBuilderFactory;
+
+    public JobConfig(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+    }
+
+    @Bean
+    public Job helloWorldJob() {
+        return jobBuilderFactory
+                .get("helloWorldJob")
+                .start(helloWorldStep())
+                .build();
+    }
+
+    @Bean
+    public Step helloWorldStep() {
+        return stepBuilderFactory
+                .get("helloWorldStep")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext)
+                            throws Exception {
+                        System.out.println(
+                                "这里是 Step 里面的 Tasklet，它可以返回 RepeatStatus.FINISHED 或 RepeatStatus.CONTINUABLE。");
+                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
+    }
+}
