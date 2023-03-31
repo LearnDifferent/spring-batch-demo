d7abf2fcd2db473ae16e78d15adb0e7e83c7256a
Zhou <zhou.h.p@outlook.com>
2023-03-31 17:04:32 +0800

    fix: change the sql

--- src/main/resources/delete-spring-batch-tables.sql
+++ src/main/resources/delete-spring-batch-tables.sql
@@ -1,11 +1,11 @@
 SET foreign_key_checks=0; -- 关闭外键检查
-drop table BATCH_JOB_INSTANCE;
-drop table BATCH_JOB_EXECUTION;
-drop table BATCH_JOB_EXECUTION_PARAMS;
-drop table BATCH_STEP_EXECUTION;
-drop table BATCH_STEP_EXECUTION_CONTEXT;
-drop table BATCH_JOB_EXECUTION_CONTEXT;
-drop table BATCH_STEP_EXECUTION_SEQ;
-drop table BATCH_JOB_EXECUTION_SEQ;
-drop table BATCH_JOB_SEQ;
+drop table if exists BATCH_JOB_INSTANCE;
+drop table if exists BATCH_JOB_EXECUTION;
+drop table if exists BATCH_JOB_EXECUTION_PARAMS;
+drop table if exists BATCH_STEP_EXECUTION;
+drop table if exists BATCH_STEP_EXECUTION_CONTEXT;
+drop table if exists BATCH_JOB_EXECUTION_CONTEXT;
+drop table if exists BATCH_STEP_EXECUTION_SEQ;
+drop table if exists BATCH_JOB_EXECUTION_SEQ;
+drop table if exists BATCH_JOB_SEQ;
 SET foreign_key_checks=1; -- 开启外键检查
